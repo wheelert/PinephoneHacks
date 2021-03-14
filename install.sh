380e66b20c88cecb8524c506d5309309eb644375
Thomas Wheeler <thomas@wheelerwire.com>
2021-03-13 22:46:52 -0800

    update

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+APPDIR=/usr/share/PinephoneHacks
+
+echo -e "\e[33m [ Checking for previous versions]\e[0m"
+if [ -d "$APPDIR" ]; then
+    rm -rf $APPDIR
+fi
+
+echo -e "\e[32m [ Installing Pinephone Hacks ]\e[0m"
+mkdir $APPDIR
+cp main.py $APPDIR
+cp PinephoneHacks.desktop /usr/share/applications/
---
+++ main.py
@@ -0,0 +1,45 @@
+import gi
+import subprocess 
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Custom Commands")
+        self.set_border_width(10)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+
+        self.button = Gtk.Button(label="Enable Data")
+        self.button.connect("clicked", self.on_button_clicked)
+        vbox.add(self.button)
+        
+        #exit button
+        self.button2 = Gtk.Button(label="Exit")
+        self.button2.connect("clicked", self.AppClose)
+        vbox.add(self.button2)
+        
+        self.add(vbox)
+        
+        
+
+    def on_button_clicked(self, widget):
+        print("Connecting WAN")
+        output = subprocess.Popen(['/usr/bin/ofonoctl', 'wan', '--connect','--ap'], text=True,
+        stdout=subprocess.PIPE)
+
+        stdout, _ = output.communicate()
+        print("output>>"+stdout)
+        
+    def AppClose(self, widget):
+        exit()
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
